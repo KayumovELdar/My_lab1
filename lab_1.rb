4324e588acf653433c705115c860986c2f087afe
KayumovEldar <robot15f462@mail.ru>
2020-11-08 03:40:24 +0300

    правка 8

--- lab_1.rb
+++ lab_1.rb
@@ -1,5 +1,5 @@
 class Route
-  attr_reader :route
+  attr_reader :route, :start_station, :finish_station
 
   def initialize(start_station, finish_station) # начало и конец маршрута
     @start_station = start_station
@@ -8,7 +8,6 @@ class Route
   end
 
   def intermediate_station(name)
-
     @route.insert(-2, name)
   end
 
@@ -17,7 +16,6 @@ class Route
   end
 
   def list_station
-    #@route.each { |station| print station.name }
     @route.each { |station| return station }
   end
 end
@@ -39,50 +37,41 @@ class Train
   end
 
   def wagon_sum # повышаем число вагонов
-    if @speed.zero?
-      @wagon += 1
-    end
+    @wagon += 1 if @speed.zero?
   end
 
   def wagon_del # понижаем число вагонов
-    if @speed.zero? && @wagon > 0
-        @wagon -= 1
-    end
+    @wagon -= 1 if @speed.zero? && @wagon > 0
   end
 
   def train_route(route)
     @route = route
     @number_station = 0
-    #puts @route.route[@number_station].name
     @route.route[@number_station].add_train(self)
   end
 
   def train_up
-    if @number_station < (@route.route.length - 1)
-      @route.route[@number_station].remove_train(self)
-      @number_station += 1
-      @route.route[@number_station].add_train(self)
-    end
+    @route.route[@number_station].remove_train(self)
+    next_station
+    @route.route[@number_station].add_train(self)
   end
 
   def train_down # перемещение назад
-    if @number_station > 0
-      @route.route[@number_station].remove_train(self)
-      @number_station -= 1
-      @route.route[@number_station].add_train(self)
-    end
+    @route.route[@number_station].remove_train(self)
+    previous_station
+    @route.route[@number_station].add_train(self)
   end
 
   def next_station
-    if (@number_station + 1) < @route.route.length
-      return @route.route[@number_station + 1]
-    end
+    return @number_station if @route.route[@number_station] == @route.finish_station
+
+    @number_station += 1
   end
 
   def previous_station
-    if (@number_station - 1) >= 0
-      return @route.route[@number_station - 1]
-    end
+    return @number_station if @route.route[@number_station] == @route.start_station
+
+    @number_station -= 1
   end
 end
 
@@ -104,51 +93,19 @@ class Station
   end
 
   def list_train
-    #@station_trains.each { |name| puts name.number  }
-    @station_trains.each { |name| return name  }
+    @station_trains.each { |name| return name }
   end
 
   def list_train_type(type_train)
     number = 0
-    train_list =[]
-    for x in @station_trains
-     if type_train == x.mode
+    train_list = []
+    @station_trains.each do |x|
+      if type_train == x.mode
         train_list += [x]
         number += 1
       end
     end
-    #train_list.each {|train| puts train.mode}
-    #puts number
-    train_list.each {|train| return train}
-    return number
+    train_list.each { |train| return train }
+    number
   end
 end
-
-=begin
-train11=Train.new("111","g",10)
-train12=Train.new("222","r",10)
-train13=Train.new("333","g",10)
-train14=Train.new("444","r",10)
-station1=Station.new("st1")
-station2=Station.new("st2")
-station3=Station.new("st3")
-station4=Station.new("st4")
-station5=Station.new("st5")
-station6=Station.new("st6")
-route1=Route.new(station1,station6)
-route1.intermediate_station(station3)
-route1.intermediate_station(station5)
-route1.list_station
-train11.train_route(route1)
-train12.train_route(route1)
-train13.train_route(route1)
-train14.train_route(route1)
-station1.list_train
-station1.list_train_type("g")
-train11.train_up
-train14.train_up
-station1.list_train
-station1.list_train_type("g")
-station3.list_train
-station3.list_train_type("g")
-=end
