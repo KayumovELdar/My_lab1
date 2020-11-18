050758eafa8c254675bedf742ba8de6bd7099985
KayumovEldar <robot15f462@mail.ru>
2020-11-18 03:20:48 +0300

    работа с вагонами

--- lesson5/wagon_cargo.rb
+++ lesson5/wagon_cargo.rb
@@ -1,7 +1,42 @@
 require_relative 'wagon'
 
 class WagonCargo < Wagon
+
+  def initialize(name,volume)
+    @name=name
+    @volume=volume
+    @volume_reserv=0
+    validate!
+  end
+
   def type
     :cargo
+  end
+
+  def take_volume(volume)
+    @volume_reserv+=volume
+    validate!
+
+  end
+
+  def reserv
+      @volume_reserv
+  end
+
+  def free
+      @volume-@volume_reserv
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false # возвращаем false, если было исключение
+  end
+
+  def validate!
+      raise "Введено пустое значение" if @name.nil?
+      raise "объем не может быть отрицательным" if @volume < 0
+      raise "перегруз" if @volume < @volume_reserv
   end
 end
--- lesson5/wagon_passenger.rb
+++ lesson5/wagon_passenger.rb
@@ -1,7 +1,51 @@
 require_relative 'wagon'
 
 class WagonPassenger < Wagon
+  FREE_SEAT="свободное место"
+  RESERV_SEAT="зарезервираванное_место"
+
+  def initialize(name,seats)
+    @name=name
+    @seats=seats
+    @seats_list={}
+    validate!
+    create_seats
+  end
+
   def type
     :passenger
   end
+
+  def create_seats
+    for i in @seats
+      @seats_list[i]=FREE_SEAT
+    end
+  end
+
+  def seat_reserv(number)
+    @seats_list[number]=RESERV_SEAT
+  end
+
+  def list_free
+    free_list=@seats_list.filter{|i| n[i]==FREE_SEAT; }
+    return free_list.lenght
+  end
+
+  def list_reserv
+    reserv_list=@seats_list.filter{|i| n[i]==RESERV_SEAT; }
+    return reserv_list.lenght
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false # возвращаем false, если было исключение
+  end
+
+  def validate!
+      raise "Введено пустое значение" if @name.nil?
+      raise "число вагонов не может быть отрицательным" if @seats < 0
+  end
+
 end
