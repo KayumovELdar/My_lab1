85065a2d3da520acd00b2983e5f464e39aa1f83a
KayumovEldar <robot15f462@mail.ru>
2020-11-25 05:56:13 +0300

    мини правки

--- lesson6/Accessor.rb
+++ lesson6/Accessor.rb
@@ -1,16 +1,15 @@
+# frozen_string_literal: true
 
 module Accessors
-
   def self.included(base)
     base.extend ClassMethods
     base.send :include, InstanceMethods
   end
 
   module ClassMethods
-
     def attr_accessor_with_history(*names)
       names.each do |name|
-        var_name="@#{name}".to_sym
+        var_name = "@#{name}".to_sym
         define_method(name) { instance_variable_get(var_name) }
 
         define_method("#{name}=".to_sym) do |value|
@@ -19,43 +18,41 @@ module Accessors
           instance_variable_set("@#{name}_history", history << value)
         end
 
-        define_method("#{name}_history".to_sym) {instance_variable_get("@#{name}_history") || []}
+        define_method("#{name}_history".to_sym) { instance_variable_get("@#{name}_history") || [] }
       end
     end
 
     def strong_attr_accessor(name, type)
-        var_name="@#{name}".to_sym
-        define_method(name) { instance_variable_get(var_name) }
+      var_name = "@#{name}".to_sym
+      define_method(name) { instance_variable_get(var_name) }
 
-        define_method("#{name}=".to_sym) do |value|
-          raise 'несовпадение типов' unless value.is_a?(type)
-          instance_variable_set(var_name, value)
-        end
+      define_method("#{name}=".to_sym) do |value|
+        raise 'несовпадение типов' unless value.is_a?(type)
+
+        instance_variable_set(var_name, value)
+      end
     end
   end
 
   module InstanceMethods
   end
-
 end
 
-=begin
-class Test
-  include Accessors
-
-  attr_accessor_with_history :my_att, :a, :b, :c
-  strong_attr_accessor :r, Integer
-end
-
-t=Test.new
-print t.instance_variables
-puts ""
-t.a = 3
-t.a = 7
-t.a = 50
-puts t.a_history
-t.r = 10
-puts t.r
-t.r="1c0"
-puts t.r
-=end
+# class Test
+#   include Accessors
+#
+#   attr_accessor_with_history :my_att, :a, :b, :c
+#   strong_attr_accessor :r, Integer
+# end
+#
+# t=Test.new
+# print t.instance_variables
+# puts ""
+# t.a = 3
+# t.a = 7
+# t.a = 50
+# puts t.a_history
+# t.r = 10
+# puts t.r
+# t.r="1c0"
+# puts t.r
--- lesson6/validate.rb
+++ lesson6/validate.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 module Validation
   def self.included(base)
@@ -6,11 +7,10 @@ module Validation
   end
 
   module ClassMethods
-
     def validation(*names, type, args)
-      h=instance_variable_get('@type_list') || []
+      h = instance_variable_get('@type_list') || []
       names.each do |name|
-      instance_variable_set('@type_list',h << [type, name,args])
+        instance_variable_set('@type_list', h << [type, name, args])
       end
     end
   end
@@ -19,43 +19,43 @@ module Validation
     def valid?
       validate!
       true
-    rescue
+    rescue StandardError
       false
     end
 
     def validate!
-      self.class.instance_variable_get('@type_list').each do |type, name,args|
-        @name=name
-        @args=args
-        #puts "#{type} #{name} #{args}"
-        send"validate_#{type}".to_sym
+      self.class.instance_variable_get('@type_list').each do |type, name, args|
+        @name = name
+        @args = args
+        # puts "#{type} #{name} #{args}"
+        send "validate_#{type}".to_sym
       end
-  end
+    end
 
-      private
+    private
 
-      def validate_type
-        raise 'не соответствие заявленному типу' unless @name.is_a?(@args)
-      end
+    def validate_type
+      raise 'не соответствие заявленному типу' unless @name.is_a?(@args)
+    end
 
-      def validate_format
-        raise "#{@name} не соответствует формату" if @name !~ @args
-      end
+    def validate_format
+      raise "#{@name} не соответствует формату" if @name !~ @args
+    end
 
-      def validate_presence
-        raise"#{@name} пустое значение" if @name==nil || @name==""
-      end
-  end
-end
-
-class Test
-  include Validation
-  validation "CCC", :presence, String
-  validation "ZZZ", :format, /^[0-9a-zA-Z]{3}(_+\d{2})*/
-  validation "CCA", :type, String
-  def initialize
-    validate!
+    def validate_presence
+      raise "#{@name} пустое значение" if @name.nil? || @name == ''
+    end
   end
 end
 
-t=Test.new
+# class Test
+#   include Validation
+#   validation "CCC", :presence, String
+#   validation "ZZZ", :format, /^[0-9a-zA-Z]{3}(_+\d{2})*/
+#   validation "CCA", :type, String
+#   def initialize
+#     validate!
+#   end
+# end
+#
+# t=Test.new
